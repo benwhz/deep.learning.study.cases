0a6aa5ff55659d3bed38c45ab3daf4c82452754c
Ben Wang <benwhz@foxmail.com>
2024-08-27 15:32:09 +0800

    update

--- Keras/tensorboard_.py
+++ Keras/tensorboard_.py
@@ -6,6 +6,8 @@ mnist = tf.keras.datasets.mnist
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
+print(y_train[0:10])
+exit()
 def create_model():
   return tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28), name='layers_flatten'),
